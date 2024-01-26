09a89c0f8500c0940bcfdecf45e88928e4ce9dc6
artistic-2 <157778571+artistic-2@users.noreply.github.com>
2024-01-26 13:15:16 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,48 +1,33 @@
-//step 1: get DOM
-let nextDom = document.getElementById('next');
-let prevDom = document.getElementById('prev');
+const header = document.querySelector("header");
 
-let carouselDom = document.querySelector('.carousel');
-let SliderDom = carouselDom.querySelector('.carousel .list');
-let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
-let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
-let timeDom = document.querySelector('.carousel .time');
+window.addEventListener ("scroll", function() {
+    header.classList.toggle ("sticky", window.scrollY > 200)
+});
 
-thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-let timeRunning = 3000;
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
 
-nextDom.onclick = function(){
-    showSlider('next');    
-}
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
 
-prevDom.onclick = function(){
-    showSlider('prev');    
-}
-let runTimeOut;
-let runNextAuto = setTimeout(() => {
-    next.click();
-}, timeAutoNext)
-function showSlider(type){
-    let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
-    let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
-    
-    if(type === 'next'){
-        SliderDom.appendChild(SliderItemsDom[0]);
-        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-        carouselDom.classList.add('next');
-    }else{
-        SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
-        thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
-        carouselDom.classList.add('prev');
-    }
-    clearTimeout(runTimeOut);
-    runTimeOut = setTimeout(() => {
-        carouselDom.classList.remove('next');
-        carouselDom.classList.remove('prev');
-    }, timeRunning);
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+};
 
-    clearTimeout(runNextAuto);
-    runNextAuto = setTimeout(() => {
-        next.click();
-    }, timeAutoNext)
-}
+
+const sr = ScrollReveal ({
+    distance: '40px',
+    duration: 2050,
+    delay: 200,
+    reset: true
+});
+
+sr.reveal('.hero-text',{origin:'top'});
+sr.reveal('.about-img, .service-item, .about-text',{origin:'bottom'});
+sr.reveal('.about-text h2, .text-center, .right-contact h2',{origin:'top'});
+sr.reveal('.left-contact',{origin:'right'});
+sr.reveal('.right-contact',{origin:'right'});
+sr.reveal('.end-section',{origin:'top'});
